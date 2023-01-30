17b1d99888e052e0a82f7d6529ac4b12663fbd3e
Gede Rico Wijaya <gricowijaya@gmail.com>
2023-01-30 17:23:18 +0800

    update the configuration of main.go

--- main.go
+++ main.go
@@ -4,20 +4,20 @@ import (
 	"database/sql"
 	"log"
 
-	db "github.com/gricowijaya/go-transaction/db/sqlc"
 	"github.com/gricowijaya/go-transaction/api"
+	db "github.com/gricowijaya/go-transaction/db/sqlc"
+	"github.com/gricowijaya/go-transaction/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	driverName     = "postgres"
-	dataSourceName = "postgresql://postgres:password@localhost:5000/user_golang?sslmode=disable"
-  serverAddress = "0.0.0.0:3940"
-)
 
 func main() {
+  config, err := util.LoadConfig(".")
+  if err != nil{ 
+    log.Fatal("Cannot check Config")
+  }
   // connect to the database
-  conn, err := sql.Open(driverName, dataSourceName)
+  conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot Connect to the Database because ", err)
 	}
@@ -25,7 +25,7 @@ func main() {
   store := db.NewStore(conn)
   server := api.NewServer(store)
 
-  err = server.Start(serverAddress)
+  err = server.Start(config.ServerAddress)
   if err != nil {
     log.Fatal("Cannot start server -> ", err)
   }
